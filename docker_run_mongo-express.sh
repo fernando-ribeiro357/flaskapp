4181cbea8d787bcbfb452b2b9db1b6659798026a
Fernando Ribeiro <fernando.ribeiro357@gmail.com>
2024-07-17 21:51:22 -0300

    removendo scripts

--- docker_run_mongo-express.sh
+++
@@ -1,7 +0,0 @@
-docker run  --detach \
-            --env ME_CONFIG_MONGODB_URL=mongodb://172.17.0.2:27017/ \
-            --env ME_CONFIG_BASICAUTH=false \
-            --name mongo-express \
-            --publish "8081:8081" \
-            --restart always \
-            mongo-express
--- docker_run_mongo.sh
+++
@@ -1,8 +0,0 @@
-docker run  --cpus 1 \
-            --detach \
-            --memory 1073741824 \
-            --name mongodb-server \
-            --publish "27017:27017" \
-            --restart always \
-            --volume mogodb_data:/data/db \
-            mongo
