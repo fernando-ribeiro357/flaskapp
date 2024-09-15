4530161cc78d12499eafdeb629e01bd83ebe87bb
Fernando Ribeiro <fernando.ribeiro357@gmail.com>
2024-09-15 01:05:09 -0300

    ajustando env-file

--- openapi/generate_openapi.sh
+++
@@ -1,1 +0,0 @@
-docker container run -it --rm --name postman-to-openapi -v $PWD:/tmp/ groundx/postman-to-openapi p2oa convert /tmp/flaskapp.postman_collection1.json /tmp/fake_env.json -o /tmp/flaskapp_openapi.yaml
