43b4f5cd0616e3eaeeaa544c079256a8294dc70a
Fernando Ribeiro <fernando.ribeiro357@gmail.com>
2024-12-16 01:52:35 -0300

    update comṕose.yaml

--- users.js
+++ users.js
@@ -3,7 +3,7 @@ db = connect('mongodb://localhost/pessoa');
 db.users.insertMany([
     {
         "name":"Administrador",
-        "password":"$pbkdf2-sha256$29000$YgzBWEtpzTmnFKJ0LgWA0A$sJG1FBiaF8rPz6V9UU6usFMI8qD0wnOijMDL2sJnt0o",
+        "password":"$pbkdf2-sha256$29000$YgzBWEtpzTmnFKJ0LgWA0A$sJG1FBiaF8rPz6V9UU6usFMI8qD0wnOijMDL2sJnt0o", /* senha123 */
         "profile":"sysadmin",
         "username":"admin",
         "email":"admin@flaskapp.local",
@@ -13,7 +13,7 @@ db.users.insertMany([
     },
     {
         "name":"Fernando Ribeiro",
-        "password":"$pbkdf2-sha256$29000$YgzBWEtpzTmnFKJ0LgWA0A$sJG1FBiaF8rPz6V9UU6usFMI8qD0wnOijMDL2sJnt0o",
+        "password":"$pbkdf2-sha256$29000$YgzBWEtpzTmnFKJ0LgWA0A$sJG1FBiaF8rPz6V9UU6usFMI8qD0wnOijMDL2sJnt0o", /* senha123 */
         "profile":"user",
         "username":"fernando",
         "email":"fernando@flaskapp.local",
@@ -23,7 +23,7 @@ db.users.insertMany([
     },
     {
         "name":"Tarsila Mancebo",
-        "password":"$pbkdf2-sha256$29000$YgzBWEtpzTmnFKJ0LgWA0A$sJG1FBiaF8rPz6V9UU6usFMI8qD0wnOijMDL2sJnt0o",
+        "password":"$pbkdf2-sha256$29000$YgzBWEtpzTmnFKJ0LgWA0A$sJG1FBiaF8rPz6V9UU6usFMI8qD0wnOijMDL2sJnt0o", /* senha123 */
         "profile":"user",
         "username":"tarsila",
         "email":"tarsila@flaskapp.local",
